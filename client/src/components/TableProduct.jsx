8a1142d7d4ff777854941b16a644c80ba952b771
Uttam Kumar <uttammaurya377@gmail.com>
2025-06-06 11:09:47 +0530

    Resolved merge conflicts

--- client/src/components/TableProduct.jsx
+++ client/src/components/TableProduct.jsx
@@ -1,51 +1,178 @@
-import React,{useContext,useEffect,useState} from 'react'
+// import React,{useContext,useEffect,useState} from 'react'
+// import AppContext from '../context/AppContext';
+
+// const TableProduct = ({ cart }) => {
+//     const {decreaseQty, addToCart, removeFromCart, clearCart } =
+//       useContext(AppContext);
+//     const [qty, setQty] = useState(0);
+//     const [price, setPrice] = useState(0);
+//     useEffect(() => {
+//       let qty = 0;
+//       let price = 0;
+//       if (cart?.items) {
+//         for (let i = 0; i < cart.items?.length; i++) {
+//           qty += cart.items[i].qty;
+//           price += cart.items[i].price;
+//         }
+//       }
+//       setPrice(price);
+//       setQty(qty);
+//     }, [cart]);
+
+    
+//   return (
+//     <>
+//       <table className="table table-bordered border-primary bg-dark text-center">
+//         <thead>
+//           <tr>
+//             <th scope="col" className="bg-dark text-light">
+//               Product Img
+//             </th>
+//             <th scope="col" className="bg-dark text-light">
+//               Title
+//             </th>
+//             <th scope="col" className="bg-dark text-light">
+//               Price
+//             </th>
+//             <th scope="col" className="bg-dark text-light">
+//               Qty
+//             </th>
+//             <th scope="col" className="bg-dark text-light cursor-pointer">
+//               Qty++
+//             </th>
+//             <th scope="col" className="bg-dark text-light cursor-pointer">
+//               Qty--
+//             </th>
+//             <th scope="col" className="bg-dark text-light cursor-pointer">
+//               remove
+//             </th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {cart?.items?.map((product) => (
+//             <tr key={product._id}>
+//               <th scope="row" className="bg-dark text-light">
+//                 <img
+//                   src={product.imgSrc}
+//                   style={{ width: "50px", height: "50px" }}
+//                 />
+//               </th>
+//               <td className="bg-dark text-light">{product.title}</td>
+//               <td className="bg-dark text-light">{product.price}</td>
+//               <td className="bg-dark text-light">{product.qty}</td>
+//               <td className="bg-dark text-light">
+//                 <span
+//                   className="material-symbols-outlined"
+//                   onClick={() =>
+//                     addToCart(
+//                       product?.productId,
+//                       product.title,
+//                       product.price / product.qty,
+//                       1,
+//                       product.imgSrc
+//                     )
+//                   }
+//                 >
+//                   add_circle
+//                 </span>
+//               </td>
+//               <td className="bg-dark text-light ">
+//                 <span
+//                   className="material-symbols-outlined "
+//                   onClick={() => decreaseQty(product?.productId, 1)}
+//                 >
+//                   do_not_disturb_on
+//                 </span>
+//               </td>
+//               <td className="bg-dark text-light">
+//                 <span
+//                   className="material-symbols-outlined"
+//                   onClick={() => {
+//                     if (confirm("Are you sure, want remove from cart")) {
+//                       removeFromCart(product?.productId);
+//                     }
+//                   }}
+//                 >
+//                   delete
+//                 </span>
+//               </td>
+//             </tr>
+//           ))}
+
+//           <tr>
+//             <th scope="row" className="bg-dark text-light"></th>
+//             <td className="bg-dark text-light">
+//               {" "}
+//               <button
+//                 className="btn btn-primary"
+//                 style={{ fontWeight: "bold" }}
+//               >
+//                 Total
+//               </button>{" "}
+//             </td>
+//             <td className="bg-dark text-light">
+//               {" "}
+//               <button
+//                 className="btn btn-warning"
+//                 style={{ fontWeight: "bold" }}
+//               >
+//                 {price}
+//               </button>
+//             </td>
+//             <td className="bg-dark text-light">
+//               <button className="btn btn-info" style={{ fontWeight: "bold" }}>
+//                 {qty}
+//               </button>
+//             </td>
+//             <td className="bg-dark text-light"></td>
+//             <td className="bg-dark text-light"></td>
+//             <td className="bg-dark text-light"></td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </>
+//   );
+// };
+
+// export default TableProduct
+
+
+
+import React, { useContext, useEffect, useState } from 'react';
 import AppContext from '../context/AppContext';
 
 const TableProduct = ({ cart }) => {
-    const {decreaseQty, addToCart, removeFromCart, clearCart } =
-      useContext(AppContext);
-    const [qty, setQty] = useState(0);
-    const [price, setPrice] = useState(0);
-    useEffect(() => {
-      let qty = 0;
-      let price = 0;
-      if (cart?.items) {
-        for (let i = 0; i < cart.items?.length; i++) {
-          qty += cart.items[i].qty;
-          price += cart.items[i].price;
-        }
-      }
-      setPrice(price);
-      setQty(qty);
-    }, [cart]);
+  const { decreaseQty, addToCart, removeFromCart, clearCart } = useContext(AppContext);
+  const [qty, setQty] = useState(0);
+  const [price, setPrice] = useState(0);
 
-    
+  useEffect(() => {
+    let qty = 0;
+    let price = 0;
+    if (cart?.items) {
+      for (let i = 0; i < cart.items.length; i++) {
+        qty += cart.items[i].qty;
+        price += cart.items[i].price;
+      }
+    }
+    setPrice(price);
+    setQty(qty);
+  }, [cart]);
+
+  const pointerStyle = { cursor: 'pointer' };
+
   return (
     <>
       <table className="table table-bordered border-primary bg-dark text-center">
         <thead>
           <tr>
-            <th scope="col" className="bg-dark text-light">
-              Product Img
-            </th>
-            <th scope="col" className="bg-dark text-light">
-              Title
-            </th>
-            <th scope="col" className="bg-dark text-light">
-              Price
-            </th>
-            <th scope="col" className="bg-dark text-light">
-              Qty
-            </th>
-            <th scope="col" className="bg-dark text-light">
-              Qty++
-            </th>
-            <th scope="col" className="bg-dark text-light">
-              Qty--
-            </th>
-            <th scope="col" className="bg-dark text-light">
-              remove
-            </th>
+            <th scope="col" className="bg-dark text-light">Product Img</th>
+            <th scope="col" className="bg-dark text-light">Title</th>
+            <th scope="col" className="bg-dark text-light">Price</th>
+            <th scope="col" className="bg-dark text-light">Qty</th>
+            <th scope="col" className="bg-dark text-light" style={pointerStyle}>Qty++</th>
+            <th scope="col" className="bg-dark text-light" style={pointerStyle}>Qty--</th>
+            <th scope="col" className="bg-dark text-light" style={pointerStyle}>Remove</th>
           </tr>
         </thead>
         <tbody>
@@ -54,6 +181,7 @@ const TableProduct = ({ cart }) => {
               <th scope="row" className="bg-dark text-light">
                 <img
                   src={product.imgSrc}
+                  alt="product"
                   style={{ width: "50px", height: "50px" }}
                 />
               </th>
@@ -63,6 +191,7 @@ const TableProduct = ({ cart }) => {
               <td className="bg-dark text-light">
                 <span
                   className="material-symbols-outlined"
+                  style={pointerStyle}
                   onClick={() =>
                     addToCart(
                       product?.productId,
@@ -76,9 +205,10 @@ const TableProduct = ({ cart }) => {
                   add_circle
                 </span>
               </td>
-              <td className="bg-dark text-light ">
+              <td className="bg-dark text-light">
                 <span
-                  className="material-symbols-outlined "
+                  className="material-symbols-outlined"
+                  style={pointerStyle}
                   onClick={() => decreaseQty(product?.productId, 1)}
                 >
                   do_not_disturb_on
@@ -87,8 +217,9 @@ const TableProduct = ({ cart }) => {
               <td className="bg-dark text-light">
                 <span
                   className="material-symbols-outlined"
+                  style={pointerStyle}
                   onClick={() => {
-                    if (confirm("Are you sure, want remove from cart")) {
+                    if (confirm("Are you sure, want to remove from cart")) {
                       removeFromCart(product?.productId);
                     }
                   }}
@@ -98,24 +229,15 @@ const TableProduct = ({ cart }) => {
               </td>
             </tr>
           ))}
-
           <tr>
             <th scope="row" className="bg-dark text-light"></th>
             <td className="bg-dark text-light">
-              {" "}
-              <button
-                className="btn btn-primary"
-                style={{ fontWeight: "bold" }}
-              >
+              <button className="btn btn-primary" style={{ fontWeight: "bold" }}>
                 Total
-              </button>{" "}
+              </button>
             </td>
             <td className="bg-dark text-light">
-              {" "}
-              <button
-                className="btn btn-warning"
-                style={{ fontWeight: "bold" }}
-              >
+              <button className="btn btn-warning" style={{ fontWeight: "bold" }}>
                 {price}
               </button>
             </td>
@@ -134,4 +256,4 @@ const TableProduct = ({ cart }) => {
   );
 };
 
-export default TableProduct
+export default TableProduct;
